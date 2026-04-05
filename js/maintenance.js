5f18d7acbec094580acfa820ee4e25212a389e07
8lat <90806701+8lat@users.noreply.github.com>
2026-04-04 21:31:32 -0700

    `

--- js/maintenance.js
+++ js/maintenance.js
@@ -1,4 +1,6 @@
-// Redirect everything to the maintenance page
+/*
+
 if (window.location.pathname !== "/maintenance/" && window.location.pathname !== "/maintenance/index.html") {
     window.location.replace("/maintenance/"); 
 }
+    */
